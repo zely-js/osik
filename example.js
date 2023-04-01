e865aeed5b12fb7b340091e797eb694a542ecefe
r.ehdals <57896501+do4ng@users.noreply.github.com>
2023-04-01 22:00:14 +0900

    chore: add examples (router, static)

--- example.js
+++ example.js
@@ -1,6 +1,29 @@
 const m = require('./packages/osik/dist');
+const static = require('./packages/static/dist');
+const { Router } = require('./packages/router/dist');
 
 const app = m.osik();
+
+const router = new Router();
+const usersRouter = new Router('/user');
+// router
+
+router.get('/hello', (req, res) => {
+  res.body = 'Hello World!';
+});
+
+usersRouter.get('/:user', (req, res) => {
+  res.body = `Hello ${req.params.user}`;
+});
+
+app.use(router.routes());
+app.use(usersRouter.routes());
+
+// static
+
+app.use(static.Static('./packages'));
+
+// middleware
 
 app.use(async (req, res, next) => {
   await next();
@@ -18,4 +41,6 @@ app.use(async (req, res) => {
   res.end('hello');
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('http://localhost:3000');
+});
